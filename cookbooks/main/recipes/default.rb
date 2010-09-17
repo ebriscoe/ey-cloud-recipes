4da4f22463bec943242b2f062e1f5083682ec8d1
Briscoe Rodgers <briscoe@mealdetails.com>
2010-09-17 11:50:32 -0400

    add sphinx everywhere

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -14,9 +14,7 @@
 # require_recipe "mbari-ruby"
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
-if node[:name] == "Utility1"
-  require_recipe "sphinx"
-end
+require_recipe "sphinx"
 
 if node[:name] == "Utility1"
   require_recipe "delayed_job"
