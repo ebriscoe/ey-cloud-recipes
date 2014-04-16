49d231c67012405728f3f0c307a825b11bb7d0c8
Jason Squatrito <jason@ezcater.com>
2014-04-16 08:20:48 -0400

    Sync with upstream and enable PostGIS for 9.3

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -117,7 +117,7 @@ include_recipe "memcached"
 
 #enable Extension modules for a given Postgresql database
 if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
-  # Extensions that support both Postgres 9.0, 9.1 and 9.2
+  # Extensions that support Postgres >= 9.0
   # postgresql9_autoexplain "dbname"
   # postgresql9_btree_gin "dbname"
   # postgresql9_btree_gist "dbname"
@@ -137,11 +137,10 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_pg_trgm "dbname"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
-
+  
   # PostGis 1.5 (use with versions 9.0, 9.1, 9.2)
-  # postgresql9_postgis "dbname"
-
-  # PostGis 2.0 (use with version 9.2)
+  # postgresql9_postgis "dbname"  
+  # PostGis 2.0 (use with versions >= 9.2)
   postgresql9_postgis2 "md2"
   # postgresql9_seg "dbname"
   # postgresql9_sslinfo "dbname"
@@ -149,16 +148,17 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
   # postgresql9_uuid_ossp "dbname"
-
-
+  
+  
   # 9.1 and 9.2 Extensions
   # postgresql9_file_fdw "dbname"
   # postgresql9_xml2 "dbname"
-
-  # 9.2 Extensions
+  
+  #9.2 Extensions
   # postgresql9_pg_stat_statements "dbname"
-
-  #Admin-Level Contribs
+  
+  # Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
+
 end
